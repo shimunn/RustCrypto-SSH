c309d7d543ece793a706b5402aa19268101a4950
shimun <shimun@shimun.net>
2025-02-08 14:42:13 +0100

    feat(ssh-key): sk with_application

--- ssh-key/src/public/sk.rs
+++ ssh-key/src/public/sk.rs
@@ -50,6 +50,13 @@ impl SkEcdsaSha2NistP256 {
         DEFAULT_APPLICATION_STRING
     }
 
+    /// Set the FIDO/U2F application.
+    #[cfg(feature = "alloc")]
+    pub fn with_application(mut self, application: String) -> Self {
+        self.application = application;
+        self
+    }
+
     /// Get the FIDO/U2F application (typically `ssh:`).
     #[cfg(feature = "alloc")]
     pub fn application(&self) -> &str {
@@ -150,6 +157,13 @@ impl SkEd25519 {
     #[cfg(not(feature = "alloc"))]
     pub fn application(&self) -> &str {
         DEFAULT_APPLICATION_STRING
+    }
+
+    /// Set the FIDO/U2F application.
+    #[cfg(feature = "alloc")]
+    pub fn with_application(mut self, application: String) -> Self {
+        self.application = application;
+        self
     }
 
     /// Get the FIDO/U2F application (typically `ssh:`).
